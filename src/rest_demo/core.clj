cf24960b7de6561e2db45101c72188e6103aad8f
Sukhryn Alexandr <alexandrvirtual@gmail.com>
2023-06-08 22:20:46 +0300

    add demo

--- src/rest_demo/core.clj
+++ src/rest_demo/core.clj
@@ -1,7 +1,68 @@
 (ns rest-demo.core
+  (:require [org.httpkit.server :as server]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer :all]
+            [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.data.json :as json])
   (:gen-class))
 
+(def people-collection (atom []))
+
+;Collection Helper functions to add a new person
+(defn addperson [firstname surname]
+  (swap! people-collection conj {:firstname (str/capitalize firstname) :surname (str/capitalize surname)}))
+
+(defn getparameter [req pname] (get (:params req) pname))
+
+(defn addperson-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (-> (let [p (partial getparameter req)]
+                  (str (json/write-str (addperson (p :firstname) (p :surname))))))})
+
+; Return List of People
+(defn people-handler [req]
+  {:status  200
+   :headers {"Content-Type" "text/json"}
+   :body    (str (json/write-str @people-collection))})
+
+; Simple Body Page
+(defn simple-body-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "Hello World"})
+
+; request-example
+(defn request-example [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (->>
+              (pp/pprint req)
+              (str "Request Object: " req))})
+
+(defn hello-name [req] ;(3)
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (->
+              (pp/pprint req)
+              (str "Hello " (:name (:params req))))})
+(defroutes app-routes
+           (GET "/" [] simple-body-page)
+           (GET "/request" [] request-example)
+           (GET "/hello" [] hello-name)
+           (GET "/people" [] people-handler)
+           (GET "/people/add" [] addperson-handler)
+           (route/not-found "Error, page not found!"))
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "This is our main entry point"
   [& args]
-  (println "Hello, World!"))
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
+    ; Run the server with Ring.defaults middleware
+    (server/run-server (wrap-defaults #'app-routes site-defaults) {:port port})
+    ; Run the server without ring defaults
+    ;(server/run-server #'app-routes {:port port})
+    (println (str "Running webserver at http:/127.0.0.1:" port "/"))))
